bdaab204c9d622442c073e220d8859dc140841a4
Arne-Christian Blystad <arne.christian@blystad.me>
2018-02-19 14:22:20 +0100

    Stop including Nexus/Pixel sysconfig xmls on all devices
    This fixes issues where any device building with OpenGapps would
    believe it's a Google Pixel device.
    
    Fixes issue #134

--- opengapps-files.mk
+++ opengapps-files.mk
@@ -1,6 +1,46 @@
 # Always needed
-PRODUCT_COPY_FILES += $(call gapps-copy-to-system,all,etc)
 PRODUCT_COPY_FILES += $(call gapps-copy-to-system,all,framework)
+
+GAPPS_NEXUS_CODENAMES += \
+    crespo \
+    crespo4g \
+    maguro \
+    toro \
+    toroplus \
+    grouper \
+    tilapia \
+    manta \
+    mako \
+    flo \
+    deb \
+    hammerhead \
+    flounder \
+    shamu \
+    angler
+
+GAPPS_PIXEL_CODENAMES += \
+    marlin \
+    muskie \
+    taimen \
+    wahoo
+
+gapps_etc_files := $(call gapps-copy-to-system,all,etc)
+
+# Remove google_build.xml on non-Pixel devices
+ifeq ($(filter $(GAPPS_PIXEL_CODENAMES),$(TARGET_PRODUCT)),)
+  gapps_etc_files := $(filter-out %sysconfig/google_build.xml,$(gapps_etc_files))
+endif
+
+# Remove nexus.xml on non-Nexus devices
+ifeq ($(filter $(GAPPS_NEXUS_CODENAMES),$(TARGET_PRODUCT)),)
+  gapps_etc_files := $(filter-out %sysconfig/nexus.xml,$(gapps_etc_files))
+endif
+
+# This is included as part of GoogleDialer build, for devices that have the
+# GoogleDialer
+gapps_etc_files := $(filter-out %sysconfig/dialer_experience.xml,$(gapps_etc_files))
+
+PRODUCT_COPY_FILES += $(gapps_etc_files)
 
 # check if we are building a vendor image
 ifneq ($(CALLED_FROM_SETUP),true)
@@ -21,3 +61,6 @@ endif
 endif
   PRODUCT_COPY_FILES += $(call gapps-copy-to-system,all,usr/srec)
 endif
+
+# Reset internal variables
+gapps_etc_files :=
